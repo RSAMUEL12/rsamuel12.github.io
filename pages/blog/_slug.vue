fc33fb1bb78c185f766a39a357b3a6c578385803
Rohan Samuel <rohansamm@gmail.com>
2022-05-06 22:29:19 +0100

    feat: add dynamic blog post slug page

---
+++ pages/blog/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <article>
+    <nuxt-content :document="article" />
+    <p>Post last updated: {{ article.updatedAt }}</p>
+    <pre> {{ article }} </pre>
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch();
+
+    return { article };
+  },
+};
+</script>
