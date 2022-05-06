1b556ce03a0c1bc075dba4036492904beb422c20
RSAMUEL12 <RSAMUEL12@users.noreply.github.com>
2022-05-06 23:12:06 +0000

    deploy: 795ac7ae37ceec5268fb3954c9c905c121f836ec

--- _nuxt/static/1651878665/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Frsamuel12.github.io\u002F_nuxt\u002Fstatic\u002F1651878665",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Frsamuel12.github.io\u002F",assetsPath:"\u002Frsamuel12.github.io\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"31d7d7a9"}}}}(null));
