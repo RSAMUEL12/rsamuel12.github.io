8ec447484628bbe497e7bb37134e486c961df80b
Rohan Samuel <rohansamm@gmail.com>
2022-05-13 21:22:49 +0100

    feat: add basic home elements

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,41 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Rohan Samuel</h1>
+  <div class="h-screen bg-[#D80032]">
+    <div class="container mx-auto">
+      <div class="columns-4 p-5">
+        <p
+          class="text-lg text-[#2B2D42] text-center transition-colors hover:text-[#EDF2F4]"
+        >
+          Rohan Samuel
+        </p>
+        <p
+          class="text-lg text-[#2B2D42] text-center transition-colors hover:text-[#EDF2F4]"
+        >
+          Developer
+        </p>
+        <p
+          class="text-lg text-[#2B2D42] text-center transition-colors hover:text-[#EDF2F4]"
+        >
+          Based in London
+        </p>
+        <a href="https://github.com/RSAMUEL12"
+          ><p
+            class="text-lg text-[#2B2D42] text-center transition-colors hover:text-[#EDF2F4]"
+          >
+            GitHub
+          </p></a
+        >
+      </div>
+      <div class="p-5 flex flex-row items-center">
+        <img src="@/assets/images/rsdev-mascot.png" width="200" height="200" />
+        <div class="flex flex-col">
+          <p class="text-5xl font-bold text-[#2B2D42] uppercase">
+            Rohan Samuel
+          </p>
+          <p class="text-2xl text-[#2B2D42]">
+            Full Stack Developer @ Theodo UK
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
